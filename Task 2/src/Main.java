8fb892918a332348977a87f2f057359604ae7632
rakhat04 <akmyrzaevrakhatr@gmail.com>
2025-01-23 14:59:04 +0500

    1. for циклы while мен ауыстырылды

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for(int fired = 2; fired <= 50; fired = fired + 2){
-            System.out.println("Уволен айтишник с id " + fired);
+        int id = 2;
+        while(id <= 50){
+            System.out.println("Уволен айтишник с id " + id);
+            id += 2;
         }
     }
 }
+

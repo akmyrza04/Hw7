90088719ed85a46755837a0142c59c66b7e91857
rakhat04 <akmyrzaevrakhatr@gmail.com>
2025-01-23 12:03:37 +0500

    1. Флаг boolean енгіздім, басынан жалған деп 2. Ингредиенттерге int переменның жасадым 0; 3. Текст шығарып, қолданушы өзі енгізетіндей Scanner; 4. Шарт қойдым, рецептпен жасалу үшін үлкен н/е тең болу қажет деп; 5. Шартқа флагты true деп жаздым, шарт орындалған жағдайда бұл шын екінін дәлелдеуге; 6. ! еместі қосу арқылы, инргердиент жетпеген жағдайда қолданушыға ескерте алатындай болуы үшін;

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,18 +1,41 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int toadEyesCount = 0;          //Жабьи глаза
-        int ghoulTearsCount = 0;        //Слезы вурдалака
-        int ravenBonesCount = 0;        //Кости ворона
-        int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        boolean isCanMake = false;
+        int toadEyesCount = 0;
+        int ghoulTearsCount = 0;
+        int ravenBonesCount = 0;
+        int dumplingsCount = 0;
 
-        //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Введите количество жабьи глазов: ");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите количество слезы вурдалака: ");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите количество кости ворона: ");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите количество пельмени: ");
+        dumplingsCount = new Scanner(System.in).nextInt();
 
-        //Рецепты Эликсиров:
-        // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
-        // 2. 2 кости ворона + 4 пельменя - Эликсир стойкости
-        // 3. 7 слез вурдалака + 1 пельмень + 2 жабьих глаза - Эликсир скрытности
-        // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
-
-        //todo дописать логику программы сюда
+        if(toadEyesCount >= 3 && ravenBonesCount >= 1) {
+            System.out.println("\n\nВы сможете сделать: Эликсир зоркости");
+            isCanMake = true;
+        }
+        if(ravenBonesCount >= 2 && dumplingsCount >= 4) {
+            System.out.println("Вы сможете сделать: Эликсир стойкости");
+            isCanMake = true;
+        }
+        if((ghoulTearsCount >= 7 && dumplingsCount >= 1) && toadEyesCount >= 2) {
+            System.out.println("Вы сможете сделать: Эликсир скрытности");
+            isCanMake = true;
+        }
+        if((ravenBonesCount >= 3 && dumplingsCount >= 10) && (ghoulTearsCount >= 5 && toadEyesCount >= 4)) {
+            System.out.println("Вы сможете сделать: Запретный эликсир");
+            isCanMake = true;
+        }
+        if (!isCanMake){
+            System.out.println("К сожалению у вас не хватает ингредиентов");
+        }
     }
 }
